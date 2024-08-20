2273ac8376a1b8b37a2550bb659a78b3d6837243
SeungJu0406 <nss8164@gmail.com>
2024-08-21 00:50:47 +0900

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.GetInstance();
+
+            string test = Singleton.instance.test;
+            Console.WriteLine(test);
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        public static Singleton instance;
+
+        public string test = "싱글톤";
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
