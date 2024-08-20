4febeb51c65e190f518065e113a18452ccb1e052
SeungJu0406 <nss8164@gmail.com>
2024-08-21 00:38:23 +0900

    Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prototype prototype = new Prototype();
+            Console.WriteLine($"{prototype.field1}");
+
+            Prototype copy = prototype.Clone();
+            Console.WriteLine($"{copy.field1}");
+        }
+    }
+}
---
+++ Prototype/Prototype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Prototype : IPrototype
+    {
+        public string field1 {  get; private set; }
+
+        public Prototype()
+        {
+            this.field1 = "도라에몽";
+        }
+        public Prototype(Prototype prototype)
+        {
+            this.field1 = prototype.field1;
+        }
+
+        public Prototype Clone()
+        {
+            return new Prototype(this);
+        }
+    }
+}
