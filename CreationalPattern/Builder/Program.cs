2cede29a508ede1ed426d9f16953861f92979e7b
SeungJu0406 <nss8164@gmail.com>
2024-08-22 00:42:49 +0900

    Builder

---
+++ CreationalPattern/Builder/Program.cs
@@ -0,0 +1,27 @@
+﻿using Builder.Builder;
+using Builder.Product;
+
+namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+            Builder1 builder1 = new Builder1();           
+          
+            director.ChangeBuilder(builder1);
+            director.Make(IsPhoneNumber.No);
+            Product1 product1 = builder1.GetResult();
+            Console.WriteLine($"{product1.name}, {product1.age}, {product1.phoneNumber}");
+
+
+            Builder2 builder2 = new Builder2();
+
+            director.ChangeBuilder(builder2);
+            director.Make(IsPhoneNumber.Yes);
+            Product2 product2 = builder2.GetResult();
+            Console.WriteLine($"{product2.name}, {product2.age}, {product2.phoneNumber}");
+        }
+    }
+}
