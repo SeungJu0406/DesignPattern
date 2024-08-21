bf88a843b2be5c7114da68167a67959536618aaa
SeungJu0406 <nss8164@gmail.com>
2024-08-22 00:39:54 +0900

    AbstractFactory

---
+++ CreationalPattern/AbstractFactory/Factory/GoldFactory.cs
@@ -0,0 +1,29 @@
+﻿using AbstractFactory.A;
+using AbstractFactory.B;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Factory
+{
+    public class GoldFactory : IAbstractFactory
+    {
+        public ProductAxe CreateProductA()
+        {
+            ProductAxe axe = new ProductAxe1();
+            axe.name = "금";
+            axe.type = "도끼";
+            return axe;
+        }
+
+        public ProductSword CreateProductB()
+        {
+            ProductSword sword = new ProductSword1();
+            sword.name = "금";
+            sword.type = "검";
+            return sword;
+        }
+    }
+}
---
+++ CreationalPattern/AbstractFactory/Interface.cs
@@ -0,0 +1,17 @@
+﻿using AbstractFactory.A;
+using AbstractFactory.B;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public interface IAbstractFactory
+    {
+        public ProductAxe CreateProductA();
+
+        public ProductSword CreateProductB();
+    }
+}
